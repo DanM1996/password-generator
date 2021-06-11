9335f4692dbd7569f1dbc7ed58ec5587c1c98864
Dan <bmdmartinez@gmail.com>
2021-06-11 16:25:43 -0400

    Password character length prompt working

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,23 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
+};
+function generatePassword() {
+  var passwordLength = prompt("Choose a length for your password between 8 and 128 characters");
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    prompt("Would you like your password to contain lowercase letters?")
+    console.log("this works");
+  }  
+  else {
+      alert("Please enter a valid option");
+      // reset the function until the user enters a proper value
+      console.log("still stuck");
+      return generatePassword();
+      
+    }; 
+    
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
